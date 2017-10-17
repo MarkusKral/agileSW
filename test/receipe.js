31febc6106d54092615abe212033e4a643395792
Markus Kral <m.kral@plan-net.com>
2017-10-17 17:07:26 +0200

    Initializing project.
    Commented needed tests for users and receipes with all there methods,
    including whether authentication is needed for that test or not.

---
+++ test/receipe.js
@@ -0,0 +1,13 @@
+/**
+
+This is the file for receipe-tests.
+
+We need:
+
+- create (auth needed)
+- show all receipes (without auth)
+- list one receipe (without auth)
+- update receipe (auth needed)
+- delete receipe (auth needed)
+
+*/
---
+++ test/user-test.js
@@ -0,0 +1,13 @@
+/**
+
+This is the file for testing users.
+We need:
+
+- create (without auth)
+- authenticate
+- edit fields (authenticated)
+- logout (authenticated)
+- delete user (authenticated)
+- show user infos (authenticated)
+
+*/
